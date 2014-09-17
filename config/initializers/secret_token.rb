8a3263e173b40d91a548cce9ec6a197164a52b07
Kristina Shindyapin <kristina.shindyapin@gmail.com>
2014-09-17 15:07:58 -0400

    Imroved README & changed sec. token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,36 @@
 # Be sure to restart your server when you modify this file.
 
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
+
+
+# Be sure to restart your server when you modify this file.
+
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FrbcApp::Application.config.secret_token = '9681f43feebbdef4a52d9b6a7a07420c0752587a67c9094033554ba031bdcc79c9c478b7483f747858e0d5b2d11e9e5c45dc35a76a3337763bc0a56f4dc93c21'
+#FrbcApp::Application.config.secret_token = '9681f43feebbdef4a52d9b6a7a07420c0752587a67c9094033554ba031bdcc79c9c478b7483f747858e0d5b2d11e9e5c45dc35a76a3337763bc0a56f4dc93c21'
